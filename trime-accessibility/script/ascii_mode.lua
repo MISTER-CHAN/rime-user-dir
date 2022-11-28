70087136ffcdfac57bb8bb4f1f7b053d775e1872
MISTER_CHAN <mister.chan@qq.com>
2022-11-28 16:47:19 +0800

    Add files via upload

--- trime-accessibility/script/ascii_mode.lua
+++ trime-accessibility/script/ascii_mode.lua
@@ -1,6 +1,5 @@
 require "import"
 
-local am = Rime.getOption("ascii_mode") and "true" or "false"
-if am ~= ... then
+if Rime.getOption("ascii_mode") and "true" or "false" ~= ... then
 	Rime.setOption("ascii_mode", ... == "true")
 end
